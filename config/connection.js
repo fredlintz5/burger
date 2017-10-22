875c458f683efb0f0588aac56e7b9d88629dcce5
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-10-22 10:19:49 -0600

    added JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,13 @@
 
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+let connection;
+
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
 	host: 'localhost',
 	port: 8889,
 	user: 'root',
